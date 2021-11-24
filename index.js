b0ca897e1f27154adb88412bd90b8c2d8c7b93c1
Connor Lawrence <connor.lawrence96@gmail.com>
2021-11-24 14:47:51 -0700

    added all tests

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const inquirer = require('inquirer');
 const generateHTML = require('./src/generateHTML');
 
-const Employee = require('./lib/employee');
-const Manager = require('./lib/manager');
-const Intern = require('./lib/intern');
-const Engineer = require('./lib/engineer');
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
 
 
 
