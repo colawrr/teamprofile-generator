f3860ab70a430297c86e6ba7121751cb4d687340
Connor Lawrence <connor.lawrence96@gmail.com>
2021-11-27 18:42:41 -0700

    added manager questions

--- index.js
+++ index.js
@@ -36,6 +36,33 @@ const addManager = () => {
                     return true;
                 }
             }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Please enter the manager's email.",
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true;
+                } else {
+                    console.log ('Please enter an email!')
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "Please enter the manager's office number",
+            validate: nameInput => {
+                if  (isNaN(nameInput)) {
+                    console.log ('Please enter an office number!')
+                    return false; 
+                } else {
+                    return true;
+                }
+            }
         }
 
     ])
