325220aa2dceb200b001618f702c46d310c57bb0
Connor Lawrence <connor.lawrence96@gmail.com>
2021-11-27 18:38:53 -0700

    add html structure

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-const inquirer = require('inquirer');
 const generateHTML = require('./src/generateHTML');
 
-const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
 
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+const teamArray = [];
+
+const addManager = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Who is the team managers name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ("Please enter the managers name.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the team managers id number?',
+            validate: nameInput => {
+                if (isNaN(nameInput)) {
+                    console.log ('Please enter the managers id number!')
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }
+
+    ])
+}
 
 
 
